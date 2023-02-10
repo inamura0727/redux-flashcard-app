80117ef850c07deccb94ae6ed81c02b418218747
Inamura Tamaki <tinamura0159@gmail.com>
2023-02-10 19:07:44 +0900

    基本機能追加（追加、削除、シャッフル）

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from "react-redux";
+import {addCard, deleteCard, shuffleCard} from './redux/cardSlice'
+import { useState } from 'react';
 
 function App() {
+  const [word, setWord] = useState('')
+  const [mean, setMean] = useState('')
+
+  const cardList = useSelector((state) =>state.cards.value)
+  const dispatch = useDispatch()
+
+  // 単語を追加
+  const handleClick=()=>{
+    dispatch(addCard({
+      id: cardList.length +1,
+      word: word,
+      mean: mean
+    }))
+    setWord('')
+    setMean('')
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>React-redux-単語帳アプリ</h1>
+      </div>
+      <div className='addPost'>
+        <input 
+        type="text" 
+        placeholder='単語を書いてね！' 
+        onChange={(e)=>  setWord(e.target.value)} 
+        value={word}
+        />
+        <input 
+        type="text" 
+        placeholder='意味を書いてね！' 
+        onChange={(e)=>  setMean(e.target.value)} 
+        value={mean}
+        />
+        <button onClick={()=>handleClick()}>追加</button>
+        <hr />
+        <div className='displayCards'>
+          {cardList.map((card)=>(
+            <div key={card.id}>
+              <h1>{card.word}</h1>
+              <p>{card.mean}</p>
+              <button onClick={()=>dispatch(deleteCard({id:card.id}))}>削除</button>
+              </div>
+          ))}
+        </div>
+        <br />
+        <button onClick={()=>dispatch(shuffleCard(cardList))}>シャッフル</button>
+      </div>
     </div>
   );
 }
---
+++ src/redux/cardSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { CardData } from "./DummyData";
+
+
+
+export const cardSlice = createSlice({
+  name: 'cards',
+  initialState:{
+    value: CardData
+  },
+  reducers:{
+    addCard: (state,action) =>{
+      state.value.push(action.payload)
+    },
+    deleteCard:(state,action)=>{
+      state.value = state.value.filter((card) =>card.id !== action.payload.id)
+    },
+    shuffleCard:(state)=>{
+      let tmp =[...CardData]
+      for (let i = tmp.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [tmp[i], tmp[j]] = [tmp[j], tmp[i]];
+      }
+      state.value = tmp
+    }
+  }
+}) 
+
+export default cardSlice.reducer;
+export const {addCard, deleteCard, shuffleCard} = cardSlice.actions;
