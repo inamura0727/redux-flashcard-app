7e381f3d55b2581f7041672007504e79d4160e5e
Inamura Tamaki <tinamura0159@gmail.com>
2023-02-14 17:17:42 +0900

    CardListページの追加、リンク設置、ボタンの出しわけ

--- src/CardList.js
+++ src/CardList.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteCard } from './redux/cardSlice';
 
 function CardList() {
-  return <h1>カードリストのページ</h1>;
+  const cardList = useSelector((state) => state.cards.value);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>カードリストのページ</h1>
+      <button>
+        <a href="/">始める</a>
+      </button>
+      <div className="displayCards">
+        {cardList.map((card) => (
+          <div key={card.id}>
+            <h1>{card.word}</h1>
+            <p>{card.mean}</p>
+            <button onClick={() => dispatch(deleteCard({ id: card.id }))}>
+              削除
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CardList;
