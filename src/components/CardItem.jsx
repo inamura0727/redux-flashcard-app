05d0ab77627f2d600b5c0d01033264a3a9786704
Inamura Tamaki <tinamura0159@gmail.com>
2023-02-24 11:59:15 +0900

    CardItemの修正

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,37 +1,56 @@
+import { useEffect } from 'react';
 import { useState, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import cardSlice, { shuffleCard } from '../redux/cardSlice';
 
-export const CardItem = ({ cardList }) => {
+export const CardItem = () => {
   const [count, setCount] = useState(0);
-  const [output, setOutput] = useState([cardList[0]]);
   const [isAnswer, setIsAnswer] = useState('');
   const [isFinished, setIsFinished] = useState(false);
+  const [isShuffled, setIshuffled] = useState(false);
+  const [isFirst, setIsFirst] = useState(true);
+
+  const cards = useSelector((state) => state.cards.value);
+  const dispatch = useDispatch();
+
   const answer = useRef(null);
 
+  // APIカード情報が取得できない際はreturnを返す
+  if (!cards.length) return;
+
+  const shuffle = () => {
+    dispatch(shuffleCard(cards));
+    setIshuffled(true);
+  };
 
   const handleClikc = () => {
-    if (count === cardList.length) {
+    // カードを全部回し終えたらボタンを切り変え&「次へ」ボタンを表示させない
+    if (count === cards.length - 1) {
       setIsFinished(true);
       return;
     }
-    if (count < cardList.length) {
+    if (count < cards.length) {
+      if (isFirst) {
+        setIsFirst(false);
+      }
       setCount((prev) => prev + 1);
-      setOutput(cardList[count]);
     }
   };
 
   return (
     <div ref={answer}>
-      <h1>hello world</h1>
+      <button onClick={shuffle}>シャッフル</button>
+      {isShuffled ? <p>シャッフルされた！</p> : null}
       {isFinished ? (
         <button>
           <a href="/cardList">一覧画面へ戻る</a>
         </button>
       ) : (
-        <button onClick={handleClikc}>{output[0] ? 'スタート' : '次へ'}</button>
+        <button onClick={handleClikc}>{isFirst ? 'スタート' : '次へ'}</button>
       )}
-      <h1>{output.word}</h1>
+      <h1>{cards[count].word.S}</h1>
       <p>{isAnswer}</p>
-      <button onClick={() => setIsAnswer(output.mean)}>答え</button>
+      <button onClick={() => setIsAnswer(cards[count].mean.S)}>答え</button>
     </div>
   );
 };
