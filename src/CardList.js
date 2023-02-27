f03948f4c65dab40988e5f50e518cbbc24aee2c6
Inamura Tamaki <tinamura0159@gmail.com>
2023-02-27 15:43:11 +0900

    react-helmet導入、titleの変更

--- src/CardList.js
+++ src/CardList.js
@@ -1,17 +1,16 @@
 import React, { memo } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addFetchCard, fetchAsyncget } from './redux/cardSlice';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import EachCard from './components/EachCard';
-import { useCallback } from 'react';
 import './css/CardList.css';
+import { Helmet } from 'react-helmet';
 
 function CardList() {
   const [word, setWord] = useState('');
   const [mean, setMean] = useState('');
 
-  const cardList = useSelector((state) => state.cards.value);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -26,37 +25,45 @@ function CardList() {
   };
 
   return (
-    <div className="App">
-      <div>
-        <h1>React-redux-単語帳アプリ</h1>
+    <>
+      <Helmet>
+        <title>単語帳アプリ</title>
+      </Helmet>
+      <div className="App">
+        <div>
+          <h1>React-redux-単語帳アプリ</h1>
+        </div>
+        <h1>カードリストのページ</h1>
+        <div className="addPost">
+          <input
+            type="text"
+            placeholder="単語を書いてね！"
+            onChange={(e) => setWord(e.target.value)}
+            value={word}
+            style={{ marginRight: '10px' }}
+          />
+          <input
+            type="text"
+            placeholder="意味を書いてね！"
+            onChange={(e) => setMean(e.target.value)}
+            value={mean}
+          />
+          <button
+            className="btn btn_wrapper btn_marginLeft"
+            onClick={() => handleClick()}
+          >
+            追加
+          </button>
+          <hr />
+        </div>
+        <div className="btn_wrapper">
+          <a href="/">
+            <button className="btn btn_size_l">始める</button>
+          </a>
+        </div>
+        <EachCard />
       </div>
-      <h1>カードリストのページ</h1>
-      <div className="addPost">
-        <input
-          type="text"
-          placeholder="単語を書いてね！"
-          onChange={(e) => setWord(e.target.value)}
-          value={word}
-          style={{ marginRight: '10px' }}
-        />
-        <input
-          type="text"
-          placeholder="意味を書いてね！"
-          onChange={(e) => setMean(e.target.value)}
-          value={mean}
-        />
-        <button className="btn btn_wrapper btn_marginLeft" onClick={() => handleClick()}>
-          追加
-        </button>
-        <hr />
-      </div>
-      <div className="btn_wrapper">
-        <a href="/">
-          <button className="btn btn_size_l">始める</button>
-        </a>
-      </div>
-      <EachCard />
-    </div>
+    </>
   );
 }
 
